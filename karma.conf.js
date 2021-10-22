91231ee479496e157eefb36b0e90a2559cf0a603
Unfried Daniel <daniel.unfried@extern.a1.at>
2021-10-22 08:51:43 +0200

    created docs folder

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bookclub'),
+      dir: require('path').join(__dirname, './coverage/TechnikumBookClub'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bookclub'`, () => {
+  it(`should have as title 'TechnikumBookClub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bookclub');
+    expect(app.title).toEqual('TechnikumBookClub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bookclub app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TechnikumBookClub app is running!');
   });
 });
